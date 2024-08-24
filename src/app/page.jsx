7c155278c0f48d39ba578ab027cfa39e3e6a04d6
Kareem Braimoh <kareembrai222@gmail.com>
2024-08-24 06:56:34 +0100

    nice commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import About from "./components/about/About";
 import HomePage from "./components/home/HomePage";
 import HomeSold from "./components/houses-sold/HomeSold";
 import Listings from "./components/listings/Listings";
-import NavWhite from "./components/navbar/Navwhite";
+import NavWhite from "./components/navbar/NavWhite";
 import Quote from "./components/quote/Quote";
 
 
